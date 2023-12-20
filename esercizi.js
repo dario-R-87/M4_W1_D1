364004abf03e842f2f5c7f854ac2ecb3643fe4ab
Dario Russo <dariorusso987@gmail.com>
2023-12-20 11:55:56 +0100

    Update esercizi.js

--- esercizi.js
+++ esercizi.js
@@ -232,3 +232,69 @@ console.log(anagramCheck("salsa", "salas"));
 console.log(anagramCheck("ciao come stai", "tutto bene"));
 console.log(anagramCheck("quanti anni hai", "inna iahitnauq"));
 /*********************************************************/
+
+/*			ESERCIZIO extra 3			*/
+
+const anagramListCheck = function (arr, word) {
+  word_split = word.split("");
+  const arr_anag = [];
+
+  for(let x=0; x<arr.length; x++){
+  item_split = arr[x].split("");
+
+  if (item_split.length !== word_split.length) {
+    continue;
+  }
+
+  let index;
+  for (let i = 0; i < word_split.length; i++) {
+    index = item_split.indexOf(word_split[i]);
+    if (index >= 0) {
+      item_split.splice(index, 1);
+    }
+  }
+  if (item_split.length === 0) {
+    arr_anag.push(arr[x]);
+  }
+  }
+  return arr_anag;
+};
+console.log(anagramListCheck(["omar","roma","radio","armadio","amor","maro","rama"], "ramo"));
+/*********************************************************/
+
+
+
+
+
+
+
+/*3. Partendo da una lista di possibili anagrammi e da una parola (entrambi passati come parametri), ritorna un nuovo array contenente tutti gli anagrammi corretti della parola data.
+Per esempio, partendo da “cartine” e [”carenti”, “incerta”, “espatrio”], il valore ritornato deve essere [”carenti”, “incerta”].
+4. Partendo da una stringa passata come parametro, ritorna `true` se la stringa è palindroma o `false` se non lo è.
+5. Partendo da un numero intero (dai parametri) ritorna un numero che contenga le stesse cifre, ma in ordine contrario. Es. 189 ⇒ 981
+6. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “scala” creata con il carattere “#” e avente X scalini.
+Es.
+X = 2
+'# '
+'##'
+X = 3
+'# '
+'## '
+'###'
+7. Crea una funzione che, data una stringa come parametro, ritorni la stessa stringa, ma al contrario. Es. “Ciao” ****⇒ “oaiC”
+8. Crea una funzione che accetti un array e un numero Y come parametro. Dividi l’array in sotto-array aventi lunghezza Y.
+Es. array: [1, 2, 3, 4], y: 2 ⇒ [[ 1, 2], [3, 4]]
+array: [1, 2, 3, 4, 5], y: 4 ⇒ [[ 1, 2, 3, 4], [5]]
+9. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “piramide” create con il carattere “#” e avente X strati.
+Es.
+X = 3
+' # '
+' ### '
+'#####'
+10. Scrivi una funzione che accetti un intero N e ritorni una matrice a spirale NxN:
+Es. N = 2
+[[1, 2],[4, 3]]
+N = 3
+[[1, 2, 3],[8, 9, 4],[7, 6, 5]]
+N = 4
+[[1, 2, 3, 4],[12, 13, 14, 5],[11, 16, 15, 6],[10, 9, 8, 7]]*/
